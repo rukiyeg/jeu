75e6702693e27f44aba29bd61aebd114c884a954
rukiyeg <r.gungoroglu@gmail.com>
2018-01-02 00:19:56 +0100

    Please enter the commit message for your changes. Lines starting

--- ninja-runner/ninja.js
+++ ninja-runner/ninja.js
@@ -9,7 +9,8 @@
         ennemies = [],
         environment = [];
 
-
+    var canUseLocalStorage = window.localStorage !== null;
+    ctx.font = "15pt Arial";
 
     //localStorage????
 
@@ -19,6 +20,18 @@
     var platformBase = canvas.height - platformWidth;
     var platformSpacer = 64;
 
+    // regler les preferences de son
+    if (canUseLocalStorage) {
+        var playSound = (localStorage['game.playSound'] == true);
+
+        if (playSound) {
+            volume = 1;
+            $('.sound').addClass('sound-on').removeClass('sound-off');
+        } else {
+            volume = 0;
+            $('.sound').addClass('sound-off').removeClass('sound-on');
+        }
+    }
 
     /**
      * Charger toutes les images et les sons
@@ -26,27 +39,23 @@
 
     var assetLoader = (function () {
         this.imgs = {
-            'backdrop': 'imgs/backdrop.png',
-            'backdrop_ground': 'imgs/backdrop_ground.png',
             'bg': 'imgs/bg.png',
-            'boxCoin': 'imgs/boxCoin.png',
+            'sky': 'imgs/sky.png',
+            'backdrop': 'imgs/backdrop.png',
+            'backdrop2': 'imgs/backdrop_ground.png',
+            'water': 'imgs/water.png',
+            'grass1': 'imgs/grassMid1.png',
+            'grass2': 'imgs/grassMid2.png',
+            'avatar_normal': 'imgs/ninja.png',
             'bridge': 'imgs/bridge.png',
+            'plant': 'imgs/plant.png',
             'bush1': 'imgs/bush1.png',
             'bush2': 'imgs/bush2.png',
-            'credits-bg': 'imgs/credits-bg.png',
-            'grass': 'imgs/grass.png',
-            'grassCliffRight': 'imgs/grassCliffRight.png',
-            'grassMid1': 'imgs/grassMid1.png',
-            'grassMid2': 'imgs/grassMid2.png',
-            'menu-bg': 'imgs/menu-bg.png',
-            'ninja': 'imgs/ninja.png',
-            'plant': 'imgs/plant.png',
-            'sky': 'imgs/sky.png',
-            'slime': 'imgs/slime.png',
-            'soundOff': 'imgs/soundOff.png',
-            'soundOn': 'imgs/soundOn.png',
+            'cliff': 'imgs/grassCliffRight.png',
             'spikes': 'imgs/spikes.png',
-            'water': 'imgs/water.png'
+            'grass': 'imgs/grass.png',
+            'box': 'imgs/boxCoin.png',
+            'slime': 'imgs/slime.png'
 
         };
 
@@ -56,7 +65,7 @@
             'gameOver': 'sounds/gameOver.mp3'
         };
 
-        var assetLoaded = 0; //nb d'objets chargé
+        var assetsLoaded = 0; //nb d'objets chargé
         var nbImgs = Object.keys(this.imgs).length; //nb d'images chargé
         var nbSounds = Object.keys(this.sounds).length; //nb de sons chargé
         this.totalAsset = nbImgs + nbSounds;
@@ -64,12 +73,12 @@
 
 
         function assetLoaded(self, folder, name) {
-            assetLoaded++;
+            assetsLoaded++;
             self[folder][name].status = 'loaded';
-            assetProgress(assetLoaded, self.totalAsset);
-            if (assetLoaded === self.totalAsset) {
+            assetProgress(assetsLoaded, self.totalAsset);
+            if (assetsLoaded === self.totalAsset) {
                 clearInterval(self.checkAudio);
-                //mainMenu();
+                mainMenu();
             }
         }
 
@@ -86,23 +95,26 @@
 
 
         var self = this;
+        var src = '';
         for (var img in this.imgs) {
             if (this.imgs.hasOwnProperty(img)) {
+                src = this.imgs[img];
                 this.imgs[img] = new Image();
                 this.imgs[img].status = 'loading';
                 this.imgs[img].onload = function () {
                     assetLoaded(self, 'imgs', img);
                 };
-                this.imgs[img].src = this.imgs[img];
+                this.imgs[img].src = src;
             }
         }
 
         for (var sound in this.sounds) {
             if (this.sounds.hasOwnProperty(sound)) {
+                src = this.sounds[sound];
                 this.sounds[sound] = new Audio();
                 this.sounds[sound].status = 'loading';
                 this.sounds[sound].volume = volume;
-                this.sounds[sound].src = this.sounds[sound];
+                this.sounds[sound].src = src;
             }
         }
         var that = this;
@@ -141,6 +153,59 @@
     }
 
     var background = (function () {
+        this.sky = {};
+        this.backdrop = {};
+        this.backdrop2 = {};
+
+        this.sky.x = 0;
+        this.sky.y = 0;
+        this.sky.speed = 0.2;
+
+        this.backdrop.x = 0;
+        this.backdrop.y = 0;
+        this.backdrop.speed = 0.4;
+
+        this.backdrop2.x = 0;
+        this.backdrop2.y = 0;
+        this.backdrop2.speed = 0.6;
+
+        /**
+         * Draw the backgrounds to the screen at different speeds
+         */
+        this.draw = function () {
+            ctx.drawImage(assetLoader.imgs.bg, 0, 0);
+
+            // Pan background
+            this.sky.x -= this.sky.speed;
+            this.backdrop.x -= this.backdrop.speed;
+            this.backdrop2.x -= this.backdrop2.speed;
+
+            // draw images side by side to loop
+            ctx.drawImage(assetLoader.imgs.sky, this.sky.x, this.sky.y);
+            ctx.drawImage(assetLoader.imgs.sky, this.sky.x + canvas.width, this.sky.y);
+
+            ctx.drawImage(assetLoader.imgs.backdrop, this.backdrop.x, this.backdrop.y);
+            ctx.drawImage(assetLoader.imgs.backdrop, this.backdrop.x + canvas.width, this.backdrop.y);
+
+            ctx.drawImage(assetLoader.imgs.backdrop2, this.backdrop2.x, this.backdrop2.y);
+            ctx.drawImage(assetLoader.imgs.backdrop2, this.backdrop2.x + canvas.width, this.backdrop2.y);
+
+            // If the image scrolled off the screen, reset
+            if (this.sky.x + assetLoader.imgs.sky.width <= 0)
+                this.sky.x = 0;
+            if (this.backdrop.x + assetLoader.imgs.backdrop.width <= 0)
+                this.backdrop.x = 0;
+            if (this.backdrop2.x + assetLoader.imgs.backdrop2.width <= 0)
+                this.backdrop2.x = 0;
+        };
+
+        return {
+            sky: this.sky,
+            backdrop: this.backdrop,
+            backdrop2: this.backdrop2,
+            draw: this.draw
+        };
+
 
     })();
 
@@ -180,12 +245,34 @@
     function spawnSprites() {
 
     }
-    
-    function spawnEnvironmentSprites(){
-        
+
+    function spawnEnvironmentSprites() {
+
     }
-    
-    
+
+    function animate() {
+        if (!stop) {
+            requestAnimationFrame(animate);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            background.draw();
+
+            ticker++;
+        }
+
+    }
+    var requestAnimFrame = (function () {
+        return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback, element) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+
 
     /**
      * Check progression de chargement des objets
@@ -210,13 +297,21 @@
         water = [];
         environment = [];
         ennemies = [];
-        player = new Player();
+        //        player = new Player();
         ticker = 0;
         stop = false;
         score = 0;
-        platformHeight = 2;
-        platformLength = 15;
+        platformHeight = 3;
+        platformLength = 23;
         gapLength = 0;
+
+
+
+        animate();
+
+        assetLoader.sounds.gameOver.pause();
+        assetLoader.sounds.bg.currentTime = 0;
+        assetLoader.sounds.bg.play();
     }
 
     function gameOver() {
